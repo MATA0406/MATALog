de11d1dc7a8b50be8b69f986fdef32856c796140
MATA0406 <jinho_46@naver.com>
2019-09-27 16:53:37 +0900

    Add passportjs

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -1,4 +1,4 @@
-import { generateSecret } from '../../../utils';
+import { generateSecret, sendSecretMail } from '../../../utils';
 import { prisma } from '../../../../generated/prisma-client';
 
 export default {
@@ -8,6 +8,7 @@ export default {
       const loginSecret = generateSecret(); // Secret Key 생성
       console.log(loginSecret);
       try {
+        await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
       } catch (err) {
